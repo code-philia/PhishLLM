533a6e7dd80b73518d83e0bbc9fc6403332ccfe1
lindsey98 <kelseyliu1998@gmail.com>
2023-07-12 17:29:18 +0800

    model chain

--- model_chain/test_baseline.py
+++ model_chain/test_baseline.py
@@ -3,11 +3,12 @@ from xdriver.xutils.PhishIntentionWrapper import PhishIntentionWrapper
 from xdriver.XDriver import XDriver
 import time
 import os
-os.environ['https_proxy'] = "http://127.0.0.1:7890" # proxy
-os.environ['http_proxy'] = "http://127.0.0.1:7890" # proxy
+import pandas as pd
 
-class TestBase():
-    def __int__(self, phishintention_cls):
+class TestBaseline():
+
+    def __init__(self, phishintention_cls):
+
         self.phishintention_cls = phishintention_cls
 
     def test_phishpedia(self, URL, screenshot_path):
@@ -18,27 +19,51 @@ class TestBase():
 
         return phish_category, phish_target, str(phishpedia_runtime)
 
-    def test_phishintention(self, URL, screenshot_path, dynamic_enabled=True):
+    def test_phishintention(self, URL, screenshot_path):
 
-        start_time = time.time()
         XDriver.set_headless()
         ph_driver = XDriver.boot(chrome=True)
-        time.sleep(3)
+        time.sleep(5)
         ph_driver.set_page_load_timeout(30)
         ph_driver.set_script_timeout(60)
+        start_time = time.time()
         phish_category, phish_target, plotvis, siamese_conf, dynamic, time_breakdown, pred_boxes, pred_classes = \
             self.phishintention_cls.test_orig_phishintention(URL, screenshot_path, ph_driver)
-        ph_driver.quit()
         phishintention_runtime = time.time() - start_time
+        ph_driver.quit()
 
         return phish_category, phish_target, str(phishintention_runtime)
 
 
 if __name__ == '__main__':
+
     phishintention_cls = PhishIntentionWrapper()
-    base_cls = TestBase(phishintention_cls)
+    base_cls = TestBaseline(phishintention_cls)
 
     all_folders = [x.strip().split('\t')[0] for x in open('./datasets/dynapd_wo_validation.txt').readlines()]
+    df = pd.read_csv('./datasets/Brand_Labelled_130323.csv')
+    root_folder = './datasets/dynapd'
+    # result = './datasets/dynapd_phishpedia.txt'
+    result = './datasets/dynapd_phishintention.txt'
+
+    for hash in all_folders:
+        target_folder = os.path.join(root_folder, hash)
+        if os.path.exists(result) and hash in open(result).read():
+            continue
+
+        shot_path = os.path.join(target_folder, 'shot.png')
+        html_path = os.path.join(target_folder, 'index.html')
+        if os.path.exists(shot_path):
+            pk_info = df.loc[df['HASH'] == hash]
+            try:
+                URL = list(pk_info['URL'])[0]
+            except IndexError:
+                URL = f'http://127.0.0.5/{hash}'
+            # pred, brand, runtime = base_cls.test_phishpedia(URL, shot_path)
+            pred, brand, runtime = base_cls.test_phishintention(URL, shot_path)
+
+            with open(result, 'a+') as f:
+                f.write(hash + '\t' + str(pred) + '\t' + str(brand) + '\t' + str(runtime) + '\n')
     print(len(all_folders))
 
 
