03ce244171716d2dcdc9df5778102231313c4031
lindsey98 <kelseyliu1998@gmail.com>
2023-08-19 08:19:51 +0800

    modify brand recog model

--- brand_recognition/test_caption_llm.py
+++ brand_recognition/test_caption_llm.py
@@ -12,7 +12,7 @@ import os
 import openai
 from brand_recognition.dataloader import *
 import idna
-os.environ['OPENAI_API_KEY'] = open('./datasets/openai_key.txt').read()
+os.environ['OPENAI_API_KEY'] = open('./datasets/openai_key3.txt').read()
 
 
 
@@ -62,49 +62,49 @@ def test(result_file):
 if __name__ == '__main__':
     openai.api_key = os.getenv("OPENAI_API_KEY")
     openai.proxy = "http://127.0.0.1:7890" # proxy
-    #
-    dataset = ShotDataset(annot_path='./datasets/alexa_screenshots_orig.txt')
+
+    dataset = ShotDataset_Caption(annot_path='./datasets/alexa_screenshots_orig.txt')
     print(len(dataset))
     model = "gpt-3.5-turbo-16k"
-    result_file = './datasets/alexa_brand_testllm_u2.txt'
+    result_file = './datasets/alexa_brand_testllm_caption.txt'
 
-    # for it in tqdm(range(len(dataset))):
-    #     start_time = time.time()
-    # #
-    #     if os.path.exists(result_file) and dataset.urls[it] in open(result_file).read():
-    #         continue
-    #     url, _, html_text = dataset.__getitem__(it, True)
-    #     domain = tldextract.extract(url).domain+'.'+tldextract.extract(url).suffix
-    #     question = question_template(html_text)
-    #
-    #     with open('./brand_recognition/prompt.json', 'rb') as f:
-    #         prompt = json.load(f)
-    #     new_prompt = prompt
-    #     new_prompt.append(question)
-    #
-    #     # example token count from the OpenAI API
-    #     inference_done = False
-    #     while not inference_done:
-    #         try:
-    #             response = openai.ChatCompletion.create(
-    #                 model=model,
-    #                 messages=new_prompt,
-    #                 temperature=0,
-    #                 max_tokens=50,  # we're only counting input tokens here, so let's not waste tokens on the output
-    #             )
-    #             inference_done = True
-    #         except Exception as e:
-    #             print(f"Error was: {e}")
-    #             new_prompt[-1]['content'] = new_prompt[-1]['content'][:len(new_prompt[-1]['content']) // 2]
-    #             time.sleep(10)
-    #     total_time = time.time() - start_time
-    #
-    #     answer = ''.join([choice["message"]["content"] for choice in response['choices']])
-    #     print(answer)
-    #     with open(result_file, 'a+') as f:
-    #         f.write(url+'\t'+domain+'\t'+answer+'\t'+str(total_time)+'\n')
+    for it in tqdm(range(len(dataset))):
 
-    test(result_file) # Recall, i.e. % brand recognized = 0.7283540802213001 Precision, i.e. % brand reported correct = 0.8409453848610667
+        if os.path.exists(result_file) and dataset.urls[it] in open(result_file).read():
+            continue
+        url, _, logo_caption, logo_ocr = dataset.__getitem__(it, True)
+        domain = tldextract.extract(url).domain+'.'+tldextract.extract(url).suffix
+        question = question_template_caption(logo_caption, logo_ocr)
+
+        with open('./brand_recognition/prompt_caption.json', 'rb') as f:
+            prompt = json.load(f)
+        new_prompt = prompt
+        new_prompt.append(question)
+
+        # example token count from the OpenAI API
+        inference_done = False
+        while not inference_done:
+            try:
+                start_time = time.time()
+                response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=new_prompt,
+                    temperature=0,
+                    max_tokens=50,  # we're only counting input tokens here, so let's not waste tokens on the output
+                )
+                inference_done = True
+            except Exception as e:
+                print(f"Error was: {e}")
+                new_prompt[-1]['content'] = new_prompt[-1]['content'][:len(new_prompt[-1]['content']) // 2]
+                time.sleep(10)
+        total_time = time.time() - start_time
+
+        answer = ''.join([choice["message"]["content"] for choice in response['choices']])
+        print(answer)
+        with open(result_file, 'a+') as f:
+            f.write(url+'\t'+domain+'\t'+answer+'\t'+str(total_time)+'\n')
+
+    # test(result_file) # Recall, i.e. % brand recognized = 0.7283540802213001 Precision, i.e. % brand reported correct = 0.8409453848610667
 
     # result_lines = open(result_file).readlines()
     # pbar = tqdm(result_lines, leave=False)
